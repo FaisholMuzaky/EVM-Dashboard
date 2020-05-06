5c2cfe5518010a6097e74c557156a891030eb025
FaisholMuzaky <fmuzaki@gmail.com>
2020-05-06 16:01:43 +0700

    update Project-Administration.html
    make new file Project-Administration.html

--- js/scripts.js
+++ js/scripts.js
@@ -25,14 +25,5 @@
         if (!next.length) {
             next = $(this).siblings(':first');
         }
-        // next.children(':first-child').clone().appendTo($(this));
-
-        // for (var i = 0; i < 4; i++) {
-        //     next = next.next();
-        //     if (!next.length) {
-        //         next = $(this).siblings(':first');
-        //     }
-        //     next.children(':first-child').clone().appendTo($(this));
-        // }
     });
 })(jQuery);
